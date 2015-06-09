8bb744a76830150c08b830c1543ba7caf3c2c781
Sowmya Viswanathan <sowmya.viswanathans@gmail.com>
2015-06-09 16:36:42 +0530

    Added Spec to see if details are returned

--- test/ItemDetailsTest.java
+++ test/ItemDetailsTest.java
@@ -1,9 +1,18 @@
 import org.junit.Test;
 
-public class ItemDetailsTest {
-    @Test
-    public void shouldChooseCategory() {
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
+public class ItemDetailsTest {
+
+    @Test
+    public void shouldReturnWelcomeString(){
+       ItemDetails itemDetails = new ItemDetails(1,"bookat",12.49);
+
+        String actualMessage = itemDetails.returnDetails();
+
+        assertThat(actualMessage, is(equalTo("1bookat12.49")));
     }
 
 }
